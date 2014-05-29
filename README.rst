c2eb0344d91f44029f353a55a272d1f30c0846e3
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-05-29 05:48:34 +0000

    init repo

---
+++ README.rst
@@ -0,0 +1,49 @@
+jsonapi
+========
+
+.. image:: https://travis-ci.org/pavlov99/jsonapi.png
+    :target: https://travis-ci.org/pavlov99/jsonapi
+    :alt: Build Status
+
+.. image:: https://coveralls.io/repos/pavlov99/jsonapi/badge.png
+    :target: https://coveralls.io/r/pavlov99/jsonapi
+    :alt: Coverage Status
+
+.. image:: https://pypip.in/v/jsonapi/badge.png
+    :target: https://crate.io/packages/jsonapi
+    :alt: Version
+
+.. image:: https://pypip.in/d/jsonapi/badge.png
+    :target: https://crate.io/packages/jsonapi
+    :alt: Downloads
+
+.. image:: https://pypip.in/format/jsonapi/badge.png
+    :target: https://pypi.python.org/pypi/jsonapi/
+    :alt: Download format
+
+
+.. image:: https://pypip.in/license/jsonapi/badge.png
+    :target: https://pypi.python.org/pypi/jsonapi/
+    :alt: License
+
+
+Root docs (api docs)
+REST anti patterns http://www.infoq.com/articles/rest-anti-patterns
+
+What makes a decent API Framework?These features:
+    * Pagination
+    * Posting of data with validation
+    * Publishing of metadata along with querysets
+    * API discovery
+    * Proper HTTP response handling
+    * Caching
+    * Serialization
+    * Throttling
+    * Authentication
+    * Permissions
+      
+Proper API frameworks also need:
+    * Really good test coverage of their code
+    * Decent performance
+    * Documentation
+    * An active community to advance and support the framework
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+from jsonapi import version
+#
+# Import multiprocessing to prevent test run problem. In case of nosetests
+# (not nose2) there is probles, for details see:
+# https://groups.google.com/forum/#!msg/nose-users/fnJ-kAUbYHQ/_UsLN786ygcJ
+# http://bugs.python.org/issue15881#msg170215w
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ""
+
+setup(
+    name="jsonapi",
+    version=version,
+    packages=find_packages(),
+    # test_suite="nose2.collector.collector",
+    test_suite="nose.collector",
+    tests_require=["nose", "mock"],
+
+    # metadata for upload to PyPI
+    author="Kirill Pavlov",
+    author_email="kirill.pavlov@phystech.edu",
+    url="https://github.com/pavlov99/",
+    description="JSON API realisation",
+    long_description=read('README.rst'),
+
+    # Full list:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    license="MIT",
+)
