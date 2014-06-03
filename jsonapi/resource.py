d4965b9fd84beae775fec5b5bb128e69e9256c6c
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-06-03 08:23:41 +0000

    play with resource methods

--- jsonapi/resource.py
+++ jsonapi/resource.py
@@ -1,2 +1,12 @@
 class Resource(object):
-    pass
+    def get(self):
+        pass
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
