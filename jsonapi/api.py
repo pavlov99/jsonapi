c04f6d240c78bad6d80be5c92319972c07245a9c
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-06-07 13:11:49 +0000

    add resources map

--- jsonapi/api.py
+++ jsonapi/api.py
@@ -45,10 +45,28 @@ class API(object):
         from django.conf.urls import url
 
         urls = [
+            url(r'^$', self.map_view),
             url(r'^(?P<resource_name>\w+)/$', self.default_view),
             url(r'^(?P<resource_name>)\w+/(?P<id>[0-9]+)/$', self.default_view),
         ]
         return urls
+
+    def map_view(self, request):
+        from django.http import HttpResponse
+        import json
+
+        resource_info = {
+            "resources": [{
+                "id": index + 1,
+                "href": "{}://{}/api/{}/".format(
+                    request.META['wsgi.url_scheme'],
+                    request.META['HTTP_HOST'],
+                    resource_name
+                ),
+            } for index, resource_name in enumerate(self.resource_map)]
+        }
+        response = json.dumps(resource_info)
+        return HttpResponse(response, content_type="application/vnd.api+json")
 
     def default_view(self, request, resource_name, **kwargs):
         #location = request.META['HTTP_HOST'] + request.path
@@ -57,4 +75,4 @@ class API(object):
         resource = self.resource_map[resource_name]
         from django.http import HttpResponse
         items = resource.get(**kwargs)
-        return HttpResponse(items)
+        return HttpResponse(items, content_type="application/vnd.api+json")
