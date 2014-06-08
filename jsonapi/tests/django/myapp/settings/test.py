3226120070f55069c15d865e45483aa941def186
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-06-08 02:23:33 +0000

    update folder structure

--- jsonapi/tests/django/myapp/settings/test.py
+++ jsonapi/tests/django/myapp/settings/test.py
@@ -5,5 +5,14 @@ DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 DATABASES['default']['NAME'] = ':memory:'
 CACHES['default']['BACKEND'] = 'django.core.cache.backends.locmem.LocMemCache'
 
+INSTALLED_APPS += (
+    'django_nose',
+)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=jsonapi',
+]
+
 
 logging.info('Test settings has been loaded.')
