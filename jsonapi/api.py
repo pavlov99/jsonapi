cc0c4bc7f0fdffecade6be05c57234a51642fa51
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-06-11 10:08:10 +0000

    add functionality to chaeck resource names

--- jsonapi/api.py
+++ jsonapi/api.py
@@ -44,9 +44,26 @@ class API(object):
         """
         if resource is None:
             def wrapper(resource):
-                self.register(resource)
-                return resource
+                return self.register(resource)
             return wrapper
+
+        if resource.Meta.name in self.resource_map:
+            raise ValueError('Resource {} already registered'.format(
+                resource.Meta.name))
+
+        if resource.Meta.name_plural in self.resource_map:
+            raise ValueError(
+                'Resource plural name {} conflicts with registered resource'.
+                format(resource.Meta.name))
+
+        resource_plural_names = {
+            r.Meta.name_plural for r in self.resource_map.values()
+        }
+        if resource.Meta.name in resource_plural_names:
+            raise ValueError(
+                'Resource name {} conflicts with other resource plural name'.
+                format(resource.Meta.name)
+            )
 
         self.resource_map[resource.Meta.name] = resource
         resource.Meta.api = self
@@ -67,10 +84,27 @@ class API(object):
         urls = [
             url(r'^$', self.map_view),
             url(r'^(?P<resource_name>\w+)/$', self.default_view),
-            url(r'^(?P<resource_name>)\w+/(?P<id>[0-9]+)/$',
+            url(r'^(?P<resource_name>\w+)/(?P<id>[0-9]+)/$',
                 self.default_view),
         ]
+        #urls.extend([
+            #url(r'^(?P<resource_name>\w+)/((?P<id>[0-9]+)/)?$', self.test_view)
+        #])
+
+        #urls.extend([
+            ##url(r'^(?P<resource_name>{})/$'.format(resource_name), self.test_view)
+            #url(r'^(?P<resource_name>\w+)/$', self.test_view),
+            #url(r'^(?P<resource_name>\w+)/(?P<resource_id>\w+)/$', self.test_view),
+            ##for resource_name in self.resource_map
+        #])
         return urls
+
+    def test_view(self, request, *args, **kwargs):
+        from django.http import HttpResponse
+        print("Test View: ", args, kwargs)
+        #import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
+        pass
+        return HttpResponse(str(args) + str(kwargs))
 
     def map_view(self, request):
         """ Show information about available resources.
