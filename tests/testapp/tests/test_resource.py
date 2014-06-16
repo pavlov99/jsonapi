0beb2a0e5d85f4b0bac795cb3fc11a841deb8cd1
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-06-16 14:29:10 +0000

    serialize file to its url, not just path.

--- tests/testapp/tests/test_resource.py
+++ tests/testapp/tests/test_resource.py
@@ -416,7 +416,9 @@ class TestResource(TestCase):
         self.assertEqual(obj['date'], author.date.isoformat())
         self.assertEqual(obj['datetime'], author.datetime.isoformat())
         self.assertEqual(obj['email'], author.email)
-        self.assertEqual(obj['authorfile'], author.authorfile.url)
+        self.assertEqual(
+            obj['authorfile'],
+            "http://testserver{}".format(author.authorfile.url))
         self.assertEqual(obj['filepath'], author.filepath)
         self.assertEqual(obj['floatnum'], author.floatnum)
         self.assertEqual(obj['integer'], author.integer)
