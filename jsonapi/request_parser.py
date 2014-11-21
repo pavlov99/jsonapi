bf1074d4ee88a3ea04bed1dc7bfaa89e0d974a5d
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-11-21 08:40:10 +0000

    add tests for sort key parser

--- jsonapi/request_parser.py
+++ jsonapi/request_parser.py
@@ -2,5 +2,34 @@
 
 
 class RequestParser(object):
-    def parse(cls, request):
-        pass
+
+    """ Rarser for GET parameters.
+
+
+
+    """
+    @classmethod
+    def parse(cls, querydict):
+        """ Parse querydict data.
+
+        Define, what parameters are general and what are resource specific,
+        structure parameters. As far as common parameters for resources are
+        defined, all of the query parametres which dont match them are
+        resource specific. So parser is resource independend.
+
+        :param dict querydict: dictionary in format
+            {param: [value]}. To get querydict in Django use
+            querydict = dict(django.http.QueryDict('a=1&a=2').iterlists())
+        :return dict result: dictionary in format {key: value}.
+            key = [sort|include|page|filter]
+            sort (list or dict)
+                * If case of 'sort' key passed, value is a list of
+                fields to sort.
+
+                * In case of typed sort, such as sort[<resource>] passed, value
+                is a dict with key <resource> and value list as in 'sort' key
+                case
+
+        """
+        result = {}
+        return result
---
+++ tests/testapp/tests/test_request_parser.py
@@ -0,0 +1,41 @@
+""" Test parser of GET parameters."""
+from django.test import TestCase
+from django.http import QueryDict
+from jsonapi.request_parser import RequestParser
+
+
+class TestRequestParser(TestCase):
+
+    def test_parse_filters(self):
+        pass
+
+    def test_parse_sort_empty(self):
+        querydict = {}
+        result = RequestParser.parse(querydict)
+        self.assertEqual(result["sort"], [])
+
+    def test_parse_sort_one_param(self):
+        querydict = {"sort": ["a"]}
+        result = RequestParser.parse(querydict)
+        self.assertEqual(result["sort"], ["a"])
+
+    def test_parse_sort_two_param(self):
+        querydict = {"sort": ["a", "b"]}
+        result = RequestParser.parse(querydict)
+        self.assertEqual(result["sort"], ["a", "b"])
+
+        query = "sort=a,b"
+        querydict = dict(QueryDict(query).iterlists())
+        result = RequestParser.parse(querydict)
+        self.assertEqual(result["sort"], ["a", "b"])
+
+        query = "sort=a&sort=b"
+        querydict = dict(QueryDict(query).iterlists())
+        result = RequestParser.parse(querydict)
+        self.assertEqual(result["sort"], ["a", "b"])
+
+    def test_parse_include(self):
+        pass
+
+    def test_parse_page(self):
+        pass
