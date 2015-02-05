cb66318079d6d81b0b3e1aba97ad4266e227dc5a
Kirill Pavlov <kirill.pavlov@phystech.edu>
2015-02-05 09:19:21 +0000

    remove resource field introspection, use model field introspection instead (more powerful)

--- jsonapi/resource.py
+++ jsonapi/resource.py
@@ -33,7 +33,6 @@ Properties:
 from . import six
 import inspect
 import logging
-from django.conf import settings
 from django.core.paginator import Paginator
 from django.db import models
 
@@ -166,171 +165,6 @@ class Resource(Serializer, Deserializer, Authenticator):
         def name_plural(cls):
             return "{0}s".format(cls.name)
 
-
-    @classmethod
-    def _get_fields_own(cls, model):
-        fields = {
-            field.name: {
-                "type": Resource.FIELD_TYPES.OWN,
-                "name": field.name,
-                "related_resource": None,
-            } for field in model._meta.fields
-            if field.rel is None and (field.serialize or field.name == 'id')
-        }
-        return fields
-
-    @classmethod
-    def _get_fields_self_foreign_key(cls, model):
-        fields = {}
-        model_resource_map = cls.Meta.api.model_resource_map
-        for field in model._meta.fields:
-            if field.rel and field.rel.multiple:
-                # relationship is ForeignKey
-                related_model = field.rel.to
-                if related_model in model_resource_map:
-                    # there is resource for related model
-                    related_resource = model_resource_map[related_model]
-                    fields[related_resource.Meta.name] = {
-                        "type": Resource.FIELD_TYPES.TO_ONE,
-                        "name": field.name,
-                        "related_resource": related_resource,
-                    }
-
-        return fields
-
-    @classmethod
-    def _get_fields_others_foreign_key(cls, model):
-        fields = {}
-        model_resource_map = cls.Meta.api.model_resource_map
-        for related_model, related_resource in model_resource_map.items():
-            if related_model == model:
-                # Do not check relationship with self
-                continue
-
-            for field in related_model._meta.fields:
-                if field.rel and field.rel.to == model and field.rel.multiple:
-                    # and not issubclass(related_model, model)? <- OneToOne rel
-                    fields[related_resource.Meta.name_plural] = {
-                        "type": Resource.FIELD_TYPES.TO_MANY,
-                        "name": field.rel.related_name or "{}_set".format(
-                            get_model_name(field.model)
-                        ),
-                        "related_resource": related_resource,
-                    }
-        return fields
-
-    @classmethod
-    def _get_fields_self_many_to_many(cls, model):
-        fields = {}
-        model_resource_map = cls.Meta.api.model_resource_map
-        for field in model._meta.many_to_many:
-            related_model = field.rel.to
-            if related_model in model_resource_map:
-                # there is resource for related model
-                related_resource = model_resource_map[related_model]
-                fields[related_resource.Meta.name_plural] = {
-                    "type": Resource.FIELD_TYPES.TO_MANY,
-                    "name": field.name,
-                    "related_resource": related_resource,
-                }
-        return fields
-
-    @classmethod
-    def _get_fields_others_many_to_many(cls, model):
-        fields = {}
-        model_resource_map = cls.Meta.api.model_resource_map
-        for related_model, related_resource in model_resource_map.items():
-            if related_model == model:
-                # Do not check relationship with self
-                continue
-
-            for field in related_model._meta.many_to_many:
-                if field.rel.to == model:
-                    fields[related_resource.Meta.name_plural] = {
-                        "type": Resource.FIELD_TYPES.TO_MANY,
-                        "name": field.rel.related_name or "{}_set".format(
-                            get_model_name(field.model)
-                        ),
-                        "related_resource": related_resource,
-                    }
-        return fields
-
-    @classproperty
-    def fields(cls):
-        """ Get resource fields.
-
-        Analyze related to resource model and models related to it.
-        Method builds resource relations based on related model relationship.
-        If there is no resource for related model, corresponding field would
-        not appear in resource, because there are no rules to serialize it.
-
-        :return dict: fields with following format:
-            {
-                fieldname: {
-                    "type": ["own"|"to_one"|"to_many"],
-                    "related_resource": [None|<resource>],
-                    "name": model field name: "title, author_id, comment_set"
-                }
-            }
-
-        fieldname is given according to resource names, to follow jsonapi
-        name attribute is name of Django model field.
-
-        #1 Get fields from model own
-        #2 Get fields from model foreign keys
-        #3 Get foreign keys from other models to current
-        #4 Get many-to-many fields from model
-        #5 Get many-to-many from other models to current
-
-        """
-        model = getattr(cls.Meta, 'model', None)
-        fields = {}
-        if model is None:
-            return fields
-
-        fields.update(cls._get_fields_own(model))
-        fields.update(cls._get_fields_self_foreign_key(model))
-        fields.update(cls._get_fields_others_foreign_key(model))
-        fields.update(cls._get_fields_self_many_to_many(model))
-        fields.update(cls._get_fields_others_many_to_many(model))
-
-        if cls.Meta.fieldnames_include is not None:
-            for fieldname in cls.Meta.fieldnames_include:
-                fields[fieldname] = {
-                    "type": Resource.FIELD_TYPES.OWN,
-                    "name": fieldname,
-                    "related_resource": None,
-                }
-
-        fields = {
-            k: v for k, v in fields.items()
-            if cls.Meta.fieldnames_exclude is None or
-            v["name"] not in cls.Meta.fieldnames_exclude
-        }
-
-        return fields
-
-    @classproperty
-    def fields_own(cls):
-        return {
-            k: v for k, v in cls.fields.items()
-            if v["type"] == Resource.FIELD_TYPES.OWN
-        }
-
-    @classproperty
-    def fields_to_one(cls):
-        return {
-            k: v for k, v in cls.fields.items()
-            if v["type"] == Resource.FIELD_TYPES.TO_ONE
-        }
-
-    @classproperty
-    def fields_to_many(cls):
-        return {
-            k: v for k, v in cls.fields.items()
-            if v["type"] == Resource.FIELD_TYPES.TO_MANY
-        }
-
     @classmethod
     def get_queryset(cls, user=None, **kwargs):
         """ Get objects queryset.
@@ -370,8 +204,6 @@ class Resource(Serializer, Deserializer, Authenticator):
         """
         user = cls.authenticate(request)
         queryset = cls.get_queryset(user=user, **kwargs)
-        print(kwargs)
-
         filters = kwargs.get("filters", {})
         if kwargs.get('ids'):
             filters["id__in"] = kwargs.get('ids')
